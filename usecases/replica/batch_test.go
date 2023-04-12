ad942ebdd8ba3d1f4f40de15186c84b5d5d2f458
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2023-04-12 15:49:19 +0200

    test getting orphan object

--- usecases/replica/batch_test.go
+++ usecases/replica/batch_test.go
@@ -760,7 +760,70 @@ func TestRepairerCheckConsistencyAll(t *testing.T) {
 		assert.Equal(t, want, xs)
 	})
 
+	t.Run("OrphanObject", func(t *testing.T) {
+		var (
+			f      = newFakeFactory("C1", shard, nodes)
+			finder = f.newFinder("A")
+			ids    = []strfmt.UUID{"1", "2", "3"}
+			xs     = []*storobj.Object{
+				objectEx(ids[0], 2, shard, "A"),
+				objectEx(ids[1], 3, shard, "A"),
+				objectEx(ids[2], 1, shard, "A"),
+			}
 
+			digestR2 = []RepairResponse{
+				{ID: ids[0].String(), UpdateTime: 1},
+				{ID: ids[1].String(), UpdateTime: 3}, // latest
+				{ID: ids[2].String(), UpdateTime: 1, Deleted: true},
+			}
+			digestR3 = []RepairResponse{
+				{ID: ids[0].String(), UpdateTime: 1},
+				{ID: ids[1].String(), UpdateTime: 1},
+				{ID: ids[2].String(), UpdateTime: 4, Deleted: true}, // latest
+			}
+			directR2 = []objects.Replica{
+				replica(ids[1], 3, false),
+			}
+		)
+
+		want := setObjectsConsistency(xs, true)
+		want[2].IsConsistent = false // orphan
+
+		f.RClient.On("DigestObjects", anyVal, nodes[1], cls, shard, ids).
+			Return(digestR2, nil).
+			Once()
+		f.RClient.On("DigestObjects", anyVal, nodes[2], cls, shard, ids).
+			Return(digestR3, nil).
+			Once()
+
+		// fetch most recent objects
+		f.RClient.On("FetchObjects", anyVal, nodes[1], cls, shard, anyVal).
+			Return(directR2, nil).
+			Once()
+		// repair
+		var (
+			repairR2 = []RepairResponse{
+				{ID: ids[1].String(), UpdateTime: 1},
+			}
+
+			repairR3 = []RepairResponse{
+				{ID: ids[0].String(), UpdateTime: 1},
+				{ID: ids[1].String(), UpdateTime: 1},
+			}
+		)
+
+		f.RClient.On("OverwriteObjects", anyVal, nodes[1], cls, shard, anyVal).
+			Return(repairR2, nil).
+			Once()
+		f.RClient.On("OverwriteObjects", anyVal, nodes[2], cls, shard, anyVal).
+			Return(repairR3, nil).
+			Once()
+
+		err := finder.CheckConsistency(ctx, All, xs)
+		assert.Nil(t, err)
+		assert.Equal(t, want, xs)
+	})
+}
 
 func genInputs(node, shard string, updateTime int64, ids []strfmt.UUID) ([]*storobj.Object, []RepairResponse) {
 	xs := make([]*storobj.Object, len(ids))
